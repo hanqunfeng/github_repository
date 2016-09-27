9ca4322264d01b4127d8a4623a3d794fbc67ccf6
hanqunfeng <hanqf2008@163.com>
2016-09-27 14:46:42 +0800

    增加工具类

---
+++ src/git/demo/Utils.java
@@ -0,0 +1,11 @@
+package git.demo;
+
+public class Utils {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5678);
+		System.out.println("你好啊");
+	}
+
+}
